f729cc54c38ba8a80c07fb90b284417916d5487e
Nazrana <38925601+Nazrana@users.noreply.github.com>
2024-10-18 13:22:08 +0430

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,39 @@
+'use strict';
+
+ // On page load - replace the default user icon with the uploaded profile picture if available
+ window.onload = function() {
+    const profilePic = localStorage.getItem('profilePic');
+    if (profilePic) {
+        document.getElementById('user-icon').src = profilePic;
+    }
+};
+
+
+
+function checkLoginStatus() {
+    // Check if user data is available in localStorage - if the user is logged in
+    const username = localStorage.getItem('username');
+    
+    if (username) {
+        // If username exists - assume user is logged in and redirect to profile
+        window.location.href = 'userprofile.html';
+    } else {
+        // If no username -  redirect to login page
+        window.location.href = 'login-register-forms.html';
+    }
+}
+
+
+
+
+
+
+
+
+//login and register forms
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
-
 
 registerLink.addEventListener('click' , ()=> {
     wrapper.classList.add('active');
@@ -10,8 +42,6 @@ registerLink.addEventListener('click' , ()=> {
 loginLink.addEventListener('click' , ()=> {
     wrapper.classList.remove('active');
 });
-
-
 
 function login(){
     var email = document.getElementById('email').value;
@@ -23,3 +53,6 @@ function login(){
         alert('Invalid login. Please try again.');
     }
 }
+
+
+
